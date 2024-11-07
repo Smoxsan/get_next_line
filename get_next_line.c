17a5caaae0c92d1b19fd6752821b8d3009197e55
Smoxsan <smox@DESKTOP-C9JQ6RG>
2024-11-07 20:53:55 +0100

    first

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,15 @@
-a
+#include <stdio.h>
+
+char *get_next_line(int fd)
+{
+	char buffer[300];
+	int i;
+	int read_bytes;
+
+	while (buffer[i] != '\0')
+	{
+		read_bytes = read(fd, buffer, 300);
+		ft_printf("%c", buffer[i]);
+		i++;
+	}
+}
